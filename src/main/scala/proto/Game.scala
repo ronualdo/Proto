2f933bc73b7b6685b459e2ff9fad5c03594037e2
ronualdo <raxmac@gmail.com>
2011-07-24 09:29:16 -0300

    added world.render phase in Game loop

--- src/main/scala/proto/Game.scala
+++ src/main/scala/proto/Game.scala
@@ -3,12 +3,13 @@ package proto
 import scala.actors.Actor
 import scala.actors.Actor._
 
-class Game(val world: World) {
+class Game(val world: World, val renderer: Renderer) {
   private var gameCycleActor: Actor = _
 
   def start() {
     gameCycleActor = actor {
       world.executeNewCycle
+      world.renderUsing (renderer)
       wait(100)
       react {
         case 'execute => start
--- src/test/scala/proto/GameTest.scala
+++ src/test/scala/proto/GameTest.scala
@@ -14,13 +14,33 @@ class GameTest extends FixtureWordSpec
     "run a new world cycle every 100 miliseconds" in {
       mockCycle => import mockCycle._
       val world = mock[World]
-      
+      val renderer = mock[Renderer]
+
       expecting { expectation => import expectation._
+        allowing (world).renderUsing(renderer)
         exactly(3).of (world).executeNewCycle
       }
 
       whenExecuting{
-        val game = new Game(world)
+        val game = new Game(world, renderer)
+        game.start
+        Thread.sleep(300)
+        game.stop
+      }
+    }
+
+    "render the world every 100 miliseconds" in {
+      mockCycle => import mockCycle._
+      val world = mock[World]
+      val renderer = mock[Renderer]
+
+      expecting { expectation => import expectation._
+        allowing (world).executeNewCycle
+        exactly(3).of (world).renderUsing(renderer)
+      }
+
+      whenExecuting {
+        val game = new Game(world, renderer)
         game.start
         Thread.sleep(300)
         game.stop
