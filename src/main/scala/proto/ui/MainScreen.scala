ee3761d9282197861ef082bb08ecbfe30cce97e5
ronualdo <raxmac@gmail.com>
2011-07-24 10:45:04 -0300

    main application frame  moved from MainScreen to GameFrame

--- src/main/scala/proto/ui/MainScreen.scala
+++ src/main/scala/proto/ui/MainScreen.scala
@@ -3,12 +3,12 @@ package proto.ui
 import java.awt.{Color, GraphicsEnvironment, Dimension}
 
 import scala.swing.RichWindow.Undecorated
+
 import proto.ui.swing.{FullScreenApplication, DirectRenderingFrame}
+import proto.{Game, World}
 
 object MainScreen extends FullScreenApplication {
 
-  def top = new DirectRenderingFrame with Undecorated {
-    background = Color.BLACK
-  }
+  def top = new GameFrame
 
 }
