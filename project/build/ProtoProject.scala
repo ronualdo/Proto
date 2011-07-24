4c41bb480a41daaaf23f0e0c68694a64bbdd45fe
ronualdo <raxmac@gmail.com>
2011-07-24 10:58:15 -0300

    main class updated in project definition

--- project/build/ProtoProject.scala
+++ project/build/ProtoProject.scala
@@ -6,5 +6,5 @@ class ProtoProject(info: ProjectInfo) extends DefaultProject(info) {
   val jmock = "org.jmock" % "jmock-legacy" % "2.5.1" % "test"
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.9.0-1"
 
-  override def mainClass = Some("proto.ui.MainScreen")
+  override def mainClass = Some("proto.ui.ProtoApplication")
 }
